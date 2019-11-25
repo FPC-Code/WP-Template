a63e6475460aaabbe2469bf82ab9f7a535930cee
Galvantua <galvantua@gmail.com>
2019-11-24 21:46:43 -0500

    fix fpc_code

--- wp-content/themes/FPC_Code/sidebar.php
+++ wp-content/themes/FPC_Code/sidebar.php
@@ -1,6 +1,17 @@
-<div id="ttr_sidebar" class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-<h2 ><?php _e('Categories'); ?></h2>
-<ul > <?php wp_list_cats('sort_column=namonthly'); ?> </ul>
-<h2 ><?php _e('Archives'); ?></h2>
-<ul > <?php wp_get_archives(); ?> </ul>
-</div>
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package FPC_Code
+ */
+
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	return;
+}
+?>
+
+<aside id="secondary" class="widget-area">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</aside><!-- #secondary -->
