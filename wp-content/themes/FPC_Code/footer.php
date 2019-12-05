073feb6c77e7b451232c978db207b78a760c8dcb
Galvantua <galvantua@gmail.com>
2019-12-04 19:32:29 -0500

    add classes to things

--- wp-content/themes/FPC_Code/footer.php
+++ wp-content/themes/FPC_Code/footer.php
@@ -16,7 +16,7 @@
 
 <footer id="colophon" class="site-footer">
 	<div class="site-info">
-		<p>Proudly Powered by FPC Code</p>
+		<p class="site-info">Proudly Powered by FPC Code</p>
 	</div><!-- .site-info -->
 </footer><!-- #colophon -->
 </div><!-- #page -->
