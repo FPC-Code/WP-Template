315e0ee3a4d60e88a13a5f75a7b09bf4f47f22ee
Galvantua <galvantua@gmail.com>
2019-12-04 19:31:25 -0500

    fix php

--- wp-content/themes/FPC_Code/footer.php
+++ wp-content/themes/FPC_Code/footer.php
@@ -16,10 +16,7 @@
 
 <footer id="colophon" class="site-footer">
 	<div class="site-info">
-		<?php
-		/* translators: 1: Theme name, 2: Theme author. */
-		printf("Powered by FPC Code");
-		?>
+		<p>Proudly Powered by FPC Code</p>
 	</div><!-- .site-info -->
 </footer><!-- #colophon -->
 </div><!-- #page -->
